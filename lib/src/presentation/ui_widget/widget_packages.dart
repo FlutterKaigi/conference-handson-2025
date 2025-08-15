c39a88a67646101712409fe8cc7ff937c6faca5e
cch-robo <robo@cch-lab.com>
2025-08-15 14:25:47 +0900

    任意の UI Widget を選択できるようにするバレルパッケージを新規追加

---
+++ lib/src/presentation/ui_widget/widget_packages.dart
@@ -0,0 +1,5 @@
+// UI Widget として各ページごとの任意のパッケージをインポートできるようにするバレルパッケージです。
+export 'default/home/currently_tasks_widget.dart';
+export 'default/home/support_animations_widget.dart';
+export 'default/reading/reading_book_widget.dart';
+export 'default/settings/reading_book_settings_widget.dart';
