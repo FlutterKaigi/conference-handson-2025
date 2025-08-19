74d1cdcc11b965b364c2c207ab7b08cba1e7ca1e
cch-robo <robo@cch-lab.com>
2025-08-19 18:57:00 +0900

    標準の default を適用するパターンに再設定

--- lib/src/presentation/ui_widget/widget_packages.dart
+++ lib/src/presentation/ui_widget/widget_packages.dart
@@ -1,7 +1,7 @@
 // UI Widget として各ページごとの任意のパッケージをインポートできるようにするバレルパッケージです。
 
 // デフォルト設定 （ui_widget）
-//export 'default/widget_packages.dart';
+export 'default/widget_packages.dart';
 
 // yu_s1127 設定 （ui_widget）
-export 'hero/widget_packages.dart'; // model設置はありません。（model/default を利用します）
+//export 'hero/widget_packages.dart'; // model設置はありません。（model/default を利用します）
