019ce47a3f1e6517d0a02de73a2d3dfc5661b4e9
cch-robo <robo@cch-lab.com>
2025-08-25 12:57:41 +0900

    読了ページ進捗率を追加して、読了ページがしきい値を超えればアニメーションさせるよう機能を追加

--- lib/src/presentation/model/default/support_animations_view_model.dart
+++ lib/src/presentation/model/default/support_animations_view_model.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../domain/model/reading_book_value_object.dart';
 import '../../ui_widget/default/custom_ui/toggle_switch.dart';
 
 final NotifierProvider<SupportAnimationsViewModel, AnimationTypeEnum>
@@ -15,6 +16,10 @@ enum AnimationTypeEnum {
   none, // 何も表示しない
   cheer, // 応援アニメーション
   scolding, // 叱咤アニメーション
+  progressRate10, // 読了率 10%
+  progressRate50, // 読了率 50%
+  progressRate80, // 読了率 80%
+  progressRate100, // 読了率 100%（読了）
 }
 
 class SupportAnimationsViewModel extends Notifier<AnimationTypeEnum> {
@@ -33,6 +38,45 @@ class SupportAnimationsViewModel extends Notifier<AnimationTypeEnum> {
     // riverpod が管理する state 変数内容 ⇒ VO の初期値を生成して返却する。
     // ここでは、VO ⇒ ValueObject としてアニメーション種別の初期値を設定する。
     return animationType;
+  }
+
+  /// （読書進捗率別）アニメーション種別更新
+  ///
+  /// 読書進捗率が 100%達成か、10%,30%,50%,70%,90% を跨いでいれば、
+  /// アニメーション種別を更新して、進捗率に従ったアニメーションを表示させます。
+  ///
+  /// - [updatedBook] : 読書進捗率更新済書籍（書籍の総ページ数と現在の読了ページ数を持つ）
+  /// - [prevReadingPageNum] : 以前の読了ページ数
+  void updateAnimationTypeIfProgressChange({
+    required ReadingBookValueObject updatedBook,
+    required int prevReadingPageNum,
+  }) {
+    // 読書進捗更新前後のアニメーション種別を取得
+    final AnimationTypeEnum editedType = _checkProgressRate(
+      updatedBook.totalPages,
+      updatedBook.readingPageNum,
+    );
+    final AnimationTypeEnum prevType = _checkProgressRate(
+      updatedBook.totalPages,
+      prevReadingPageNum,
+    );
+
+    // 進捗率が更新された場合のみ、アニメーション種別を更新します。
+    if (prevType.index < editedType.index) {
+      updateAnimationType(animationType: editedType);
+    }
+  }
+
+  /// 読書進捗率チェッカー
+  AnimationTypeEnum _checkProgressRate(int totalPages, int readingPageNum) {
+    final double rate = readingPageNum / totalPages;
+    return switch (rate) {
+      < 0.1 => AnimationTypeEnum.none,
+      < 0.5 => AnimationTypeEnum.progressRate10,
+      < 0.8 => AnimationTypeEnum.progressRate50,
+      < 1.0 => AnimationTypeEnum.progressRate80,
+      _ => AnimationTypeEnum.progressRate100,
+    };
   }
 
   /// アニメーション種別更新
@@ -69,7 +113,6 @@ class SupportAnimationsViewModel extends Notifier<AnimationTypeEnum> {
           if (value) {
             // 10秒後にイベントを発行してフラグを OFF にします。
             Timer(const Duration(seconds: 10), () {
-              // FIXME アニメーション種類を増やし、表示パターンを増やすこと。
               _updateAnimationType(animationType: AnimationTypeEnum.cheer);
               updateState(value: !value);
             });
@@ -91,7 +134,6 @@ class SupportAnimationsViewModel extends Notifier<AnimationTypeEnum> {
           if (value) {
             // 10秒後にイベントを発行してフラグを OFF にします。
             Timer(const Duration(seconds: 10), () {
-              // FIXME アニメーション種類を増やし、表示パターンを増やすこと。
               _updateAnimationType(animationType: AnimationTypeEnum.scolding);
               updateState(value: !value);
             });
