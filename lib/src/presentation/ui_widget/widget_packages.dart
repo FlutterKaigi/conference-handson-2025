170f5afc78990cde6b85c148afa8cf72f2960925
cch-robo <robo@cch-lab.com>
2025-09-07 16:00:37 +0900

    UI Widget デフォルト設定を選択するように修正

--- lib/src/presentation/ui_widget/widget_packages.dart
+++ lib/src/presentation/ui_widget/widget_packages.dart
@@ -1,10 +1,10 @@
 // UI Widget として各ページごとの任意のパッケージをインポートできるようにするバレルパッケージです。
 
 // デフォルト設定 （ui_widget）
-// export 'default/widget_packages.dart';
+export 'default/widget_packages.dart';
 
 // yu_s1127 設定 （ui_widget）
 //export 'hero/widget_packages.dart'; // model設置はありません。（model/default を利用します）
 // export 'interactive_donut_chart/widget_packages.dart'; // model設置はありません。（model/default を利用します）
 // export 'morphing_button/widget_packages.dart'; // model設置はありません。（model/default を利用します）
-export 'enhanced_progress/widget_packages.dart'; // model設置はありません。（model/default を利用します）
+// export 'enhanced_progress/widget_packages.dart'; // model設置はありません。（model/default を利用します）
