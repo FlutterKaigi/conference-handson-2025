369d3067b52aeaf930691205d9d76e0f38366acd
cch-robo <robo@cch-lab.com>
2025-10-16 23:23:06 +0900

    default/homeの SupportAnimationWidget 機能分割に伴う改訂 - hero/reading_book_widget.dart: 改訂 - hero/widget_packages.dart: 改訂反映

--- lib/src/presentation/ui_widget/hero/widget_packages.dart
+++ lib/src/presentation/ui_widget/hero/widget_packages.dart
@@ -1,6 +1,7 @@
 // UI Widget として各ページごとの任意のパッケージをインポートできるようにするバレルパッケージです。
 export '../default/custom_ui/toggle_switch.dart';
-export '../default/home/support_animations_widget.dart';
+export '../default/home/reading_progress_animations_widget.dart';
+export '../default/home/reading_support_animations_widget.dart';
 export '../default/reading_graph/reading_book_graph_widget.dart';
 export '../default/settings/reading_book_settings_widget.dart';
 
