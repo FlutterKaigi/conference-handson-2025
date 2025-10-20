076ff453ef73521f8a24e55711dc2665fdde9afc
cch-robo <robo@cch-lab.com>
2025-10-21 04:06:13 +0900

    ハンズオン実装 challenge ディレクトリ新規作成 - 現時点では、完成形 complete ディレクトリをファイルコピーとなっています。

---
+++ lib/src/presentation/ui_widget/challenge/home/currently_tasks_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../domain/model/reading_books_value_object.dart';
+import '../../../../fundamental/debug/debug_logger.dart';
+import '../../../../fundamental/ui_widget/consumer_staged_widget.dart';
+import '../../../../routing/app_router.dart';
+import '../../../model/view_model_packages.dart';
+
+class CurrentlyTasksWidget
+    extends ConsumerStagedWidget<ReadingBooksValueObject, ScrollController> {
+  /// コンストラクタ
+  ///
+  /// - [provider] : 引数の Riverpod ref を使って状態値を取得する関数。
+  ///
+  /// - [builders] : （オプション）[buildList]を上書きする、
+  ///   [provider]が返した状態値に対応するビルド・メソッド一覧を返す関数。
+  ///
+  /// - [selectBuilder] : （オプション）[selectBuild]を上書きする、
+  ///   [provider]が返した状態値に対応するビルド・メソッドを返す関数。
+  const CurrentlyTasksWidget({
+    required super.provider,
+    super.builders,
+    super.selectBuilder,
+    super.key,
+  });
+
+  @override
+  // ウイジェット内部状態として ScrollController を使います。
+  ScrollController? createWidgetState() => ScrollController();
+
+  @override
+  // ignore: unnecessary_overrides
+  void initState(ScrollController? state) {
+    super.initState(state);
+  }
+
+  @override
+  void disposeState(ScrollController? state) {
+    state!.dispose();
+    super.disposeState(state);
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+    ReadingBooksValueObject value,
+    ScrollController? state,
+  ) {
+    // CurrentlyTasksWidget ウイジェットの内部状態
+    final ScrollController scrollController = state!;
+
+    return Scrollbar(
+      controller: scrollController,
+      thumbVisibility: true, // スクロールバーを常に表示（任意）
+      child: ListView.builder(
+        controller: scrollController,
+        itemCount: value.readingBooks.length,
+        itemBuilder: (BuildContext context, int index) {
+          final String title = value.readingBooks[index].name;
+          return Card(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: ListTile(
+              title: Text(title),
+              onTap: () {
+                ref
+                    .read(readingBooksProvider.notifier)
+                    .selectReadingBook(index: index);
+                debugLog('$title がタップされました。');
+                context.goReadingBook();
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
