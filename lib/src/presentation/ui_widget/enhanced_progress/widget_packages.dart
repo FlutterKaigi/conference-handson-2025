3dbfd52ea1c3170390de5070c917bb055621f2a1
cch-robo <robo@cch-lab.com>
2025-10-21 01:40:26 +0900

    カスタムトグルスイッチの fundamental移設に伴う修正

--- lib/src/presentation/ui_widget/enhanced_progress/widget_packages.dart
+++ lib/src/presentation/ui_widget/enhanced_progress/widget_packages.dart
@@ -1,5 +1,4 @@
 // UI Widget として各ページごとの任意のパッケージをインポートできるようにするバレルパッケージです。
-export '../default/custom_ui/toggle_switch.dart';
 export '../default/home/currently_tasks_widget.dart';
 export '../default/home/reading_support_animations_widget.dart';
 export '../default/reading/reading_book_widget.dart';
--- lib/src/presentation/ui_widget/hero/widget_packages.dart
+++ lib/src/presentation/ui_widget/hero/widget_packages.dart
@@ -1,5 +1,4 @@
 // UI Widget として各ページごとの任意のパッケージをインポートできるようにするバレルパッケージです。
-export '../default/custom_ui/toggle_switch.dart';
 export '../default/home/reading_progress_animations_widget.dart';
 export '../default/home/reading_support_animations_widget.dart';
 export '../default/reading_graph/reading_book_graph_widget.dart';
