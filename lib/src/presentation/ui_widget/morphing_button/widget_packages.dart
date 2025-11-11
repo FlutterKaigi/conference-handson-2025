139f80bcb96650de10d5bb7543795a76f0fc1a75
cch-robo <robo@cch-lab.com>
2025-11-12 00:55:35 +0900

    mainブランチで不要なファイルを削除

--- lib/src/presentation/ui_widget/morphing_button/widget_packages.dart
+++
@@ -1,8 +0,0 @@
-// Morphing Reading Log Button 実装のためのバレルパッケージです。
-export '../default/home/currently_tasks_widget.dart';
-export '../default/home/reading_progress_animations_widget.dart';
-export '../default/home/reading_support_animations_widget.dart';
-export '../default/reading_graph/reading_book_graph_widget.dart';
-export '../default/settings/reading_book_settings_widget.dart';
-
-export 'reading/reading_book_widget.dart';
